1abd56877e61b8cc20256508f3101dafe75da942
Tanmay Dharmaraj <get2tanmay@gmail.com>
2015-09-11 19:27:02 +0530

    Some fixes with logging

--- Fakr/cluster.js
+++ Fakr/cluster.js
@@ -4,24 +4,47 @@ winston.emitErrs = true;
 
 if (cluster.isMaster) {
     cluster.setupMaster({ silent: true }); // Keep cluster from automatically grabbing stdin/out/err
-    var cpuCount = require('os').cpus().length;
-    winston.add(winston.transports.File, {
-        level: 'info',
-        filename: './logs/all-logs.log',
-        handleexceptions: true,
-        json: true,
-        maxsize: 5242880, //5mb
-        maxfiles: 5,
-        colorize: false
+    var cpuCount = require('os').cpus().length;    
+    var logger = new winston.Logger({
+        transports: [
+            new (winston.transports.File)({
+                name: 'all-logs',
+                level: 'info',
+                filename: './logs/all-logs.log',
+                handleExceptions: false,
+                json: true,
+                maxsize: 5242880, //5MB
+                maxFiles: 5,
+                colorize: false
+            }),
+            new (winston.transports.File)({
+                name: 'error-logs',
+                level: 'error',
+                filename: './logs/error.log',
+                handleExceptions: true,
+                json: true,
+                maxsize: 5242880, //5MB
+                maxFiles: 5,
+                colorize: false
+            }),
+            new winston.transports.Console({
+                level: 'debug',
+                handleExceptions: true,
+                json: false,
+                colorize: true
+            })
+        ],
+        exitOnError: false
     });
+
     
     for (var i = 0; i < cpuCount; i++) {
-        worker = cluster.fork();
+        var worker = cluster.fork();
         worker.process.stdout.on('data', function (chunk) {
-            winston.info('worker ' + i + ': ' + chunk);
+            logger.log('info', 'worker ' + i + ': ' + chunk);
         });
         worker.process.stderr.on('data', function (chunk) {
-            winston.debug('worker ' + i + ': ' + chunk);
+            logger.log('error', 'worker ' + i + ': ' + chunk);
         });
     }
     
@@ -29,10 +52,10 @@ if (cluster.isMaster) {
     cluster.on('exit', function () {
         var worker = cluster.fork();
         worker.process.stdout.on('data', function (chunk) {
-            winston.info('worker ' + i + ': ' + chunk);
+            logger.log('info', 'worker ' + i + ': ' + chunk);
         });
         worker.process.stderr.on('data', function (chunk) {
-            winston.warn('worker ' + i + ': ' + chunk);
+            logger.log('error', 'worker ' + i + ': ' + chunk);
         });
     });
 } else {
